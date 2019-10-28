9ce79154a30ef2c6a361b285466c1c02170ea16e
edumoreira1506 <contato@eduardoem.com.br>
2019-10-27 22:26:33 -0300

    Added list os jobs

---
+++ src/main/java/br/edu/utfpr/contratedev/model/mapper/JobMapper.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.contratedev.model.mapper;
+
+import br.edu.utfpr.contratedev.dto.JobDTO;
+import br.edu.utfpr.contratedev.model.domain.Job;
+
+public class JobMapper {
+	public static Job toEntity(JobDTO dto){
+        Job entity = new Job(dto.getName(), dto.getSalary(), dto.getDescription(), dto.getCandidates());
+        return entity;
+    }
+
+    public static JobDTO toDTO(Job entity){
+    	JobDTO dto = new JobDTO(entity.getName(), entity.getSalary(), entity.getDescription(), entity.getCandidates());
+        return dto;
+    }
+}
